fa1c3824e8c3609ad0494131b3e1d44dc46a77b7
Annu Manikpuri <annu.manikpuri@invennico.com>
2026-02-24 14:15:32 +0530

    fix: updated the text overlapping in the capabilities page

--- src/app/capabilities/Hero.tsx
+++ src/app/capabilities/Hero.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-[600px] flex items-start overflow-hidden">
-      <div className="absolute inset-0 w-full h-full z-0">
+    <section className="relative w-full min-h-[700px] flex items-start overflow-hidden">
+      <div className="absolute inset-0  z-0">
         <Image
           src={HeroImg}
           alt="Industrial Worker with fabrication machinery"
@@ -20,7 +20,7 @@ const Hero = () => {
           <h1 className="text-3xl md:text-4xl lg:text-[44px] font-bold text-primary mb-5 uppercase tracking-wide">
             Engineering Capabilities
           </h1>
-          <p className="text-[#808080] text-sm md:text-base lg:text-lg max-w-3xl">
+          <p className="text-[#808080] text-sm md:text-base lg:text-lg max-w-3xl ">
             Joule Engineering Works delivers engineering-driven fabrication solutions tailored to complex industrial requirements. Our approach integrates technical precision, code compliance, and practical execution understanding to support EPC companies and industrial clients.
           </p>
         </div>
